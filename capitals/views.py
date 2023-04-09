76741a2e55f11721d710975c6c67bcde927fe9b8
Michelle Labasbas <manthanein@gmail.com>
2023-04-09 02:23:15 +0000

    working add form with bootstrap

--- capitals/views.py
+++ capitals/views.py
@@ -3,6 +3,11 @@ from django.views.generic.edit import CreateView
 from .models import Capital
 
 
+class HomePageView(ListView):
+    template_name = "home.html"
+    model = Capital
+
+
 class NewCapitalView(CreateView):
     template_name = "capital_new.html"
     model = Capital
