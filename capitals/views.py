93d678fbfb5c6f2e2f04eee83df0ceb320c017f5
Michelle Labasbas <manthanein@gmail.com>
2023-04-09 01:56:12 +0000

    added the htmls to add/update data

--- capitals/views.py
+++ capitals/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from .models import Capital
 
-# Create your views here.
+
+class NewCapitalView(CreateView):
+    template_name = "capital_new.html"
+    model = Capital
+    fields = ["city", "country"]
