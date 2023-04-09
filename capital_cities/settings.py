be757b4233646ce3aa81e5ec4abea01f5fea9cba
Michelle Labasbas <manthanein@gmail.com>
2023-04-09 03:21:18 +0000

    prep for env variables

--- capital_cities/settings.py
+++ capital_cities/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os  # only in codio
 from pathlib import Path
+from environs import Env
 
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
